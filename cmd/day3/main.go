eb1fae6508f8f51fa7471f7905eccbfc720c6b7b
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2021-12-02 23:08:22 -0800

    day 3

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day3.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -17,9 +18,152 @@ func main() {
 }
 
 func part1(input []string) int {
-	return 0
+	bits := len(input[0])
+	counters := make([]int, bits)
+	cutoff := len(input) / 2
+
+	for _, reading := range input {
+		if reading == "" {
+			continue
+		}
+		for i, c := range reading {
+			if c == '1' {
+				counters[bits-1-i]++
+			}
+		}
+	}
+
+	epsilon, gamma := 0, 0
+	for i, count := range counters {
+		if count > cutoff {
+			epsilon = epsilon | 1<<i
+		}
+		if count < cutoff {
+			gamma = gamma | 1<<i
+		}
+	}
+
+	return epsilon * gamma
+}
+
+type trieNode struct {
+	data     string
+	children map[rune]*trieNode
+}
+
+func insert(t *trieNode, s string) {
+	for _, c := range s {
+		if _, ok := t.children[c]; !ok {
+			t.children[c] = &trieNode{
+				children: make(map[rune]*trieNode),
+			}
+		}
+		t = t.children[c]
+	}
+	t.data = s
+}
+
+func traverse(t *trieNode) []string {
+	hits := make([]string, 0)
+	q := make([]*trieNode, 1)
+	q[0] = t
+
+	for len(q) > 0 {
+		head := q[0]
+		q = q[1:]
+		if head.data != "" {
+			hits = append(hits, head.data)
+			continue
+		}
+		for _, n := range head.children {
+			q = append(q, n)
+		}
+	}
+	return hits
+}
+
+func prefix(t *trieNode, s string) []string {
+	for _, c := range s {
+		if _, ok := t.children[c]; !ok {
+			return nil
+		}
+		t = t.children[c]
+	}
+
+	return traverse(t)
+}
+
+func countBit(input []string, pos int) int {
+	acc := 0
+	for _, reading := range input {
+		if reading[pos] == '1' {
+			acc++
+		}
+	}
+	return acc
+}
+
+type selector func(int, int) (rune, int)
+
+func getOxyReading(input []string, t *trieNode) int {
+	return getReading(input, t, func(hits, cutoff int) (rune, int) {
+		if hits >= cutoff {
+			return '1', 1
+		}
+		return '0', 0
+	})
+}
+
+func getCo2Reading(input []string, t *trieNode) int {
+	return getReading(input, t, func(hits, cutoff int) (rune, int) {
+		if hits >= cutoff {
+			return '0', 0
+		}
+		return '1', 1
+	})
+}
+
+func toInt(binary string) int {
+	val := 0
+	for i, c := range binary {
+		if c == '1' {
+			val = val | 1<<(len(binary)-i-1)
+		}
+	}
+	return val
+}
+
+func getReading(input []string, t *trieNode, nextBit selector) int {
+	query := strings.Builder{}
+	value := 0
+
+	bits := len(input[0])
+
+	for i := 0; i < bits; i++ {
+		if len(input) == 1 {
+			return toInt(input[0])
+		}
+		hits := countBit(input, i)
+		nextRune, bitVal := nextBit(hits, len(input)/2+len(input)%2)
+		query.WriteRune(nextRune)
+		if bitVal == 1 {
+			value = value | 1<<(bits-i-1)
+		}
+		input = prefix(t, query.String())
+	}
+
+	return value
 }
 
 func part2(input []string) int {
-	return 0
+	input = input[:len(input)-1]
+	t := &trieNode{
+		children: make(map[rune]*trieNode),
+	}
+
+	for _, reading := range input {
+		insert(t, reading)
+	}
+
+	return getCo2Reading(input, t) * getOxyReading(input, t)
 }
--- cmd/day3/main_test.go
+++ cmd/day3/main_test.go
@@ -6,12 +6,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []string{
+	"00100",
+	"11110",
+	"10110",
+	"10111",
+	"10101",
+	"01111",
+	"00111",
+	"11100",
+	"10000",
+	"11001",
+	"00010",
+	"01010",
+	"",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 198, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, 230, part2(input))
 }
