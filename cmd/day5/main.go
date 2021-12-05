610c684cbe7abe6823ac967b5b8564073e0d9bbb
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-05 09:45:32 -0800

    day 5

--- cmd/day5/main.go
+++ cmd/day5/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day5.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,10 +18,130 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
+type direction int
+
+const (
+	NorthSouth direction = 1
+	EastWest   direction = 2
+	Other      direction = 0
+)
+
+type point struct {
+	I, J int
+}
+
+func (p point) Sub(o point) point {
+	i := p.I - o.I
+	j := p.J - o.J
+	return point{i, j}
+}
+
+func (p point) Add(o point) point {
+	return point{
+		p.I + o.I,
+		p.J + o.J,
+	}
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+	return i
+}
+
+func (p point) Norm() point {
+	var n int
+	if p.I == 0 {
+		n = abs(p.J)
+	} else {
+		n = abs(p.I)
+	}
+	return point{
+		p.I / n,
+		p.J / n,
+	}
+}
+
+func (p point) Equal(o point) bool {
+	return p.I == o.I && p.J == o.J
+}
+
+func (l line) Trace() []point {
+	result := make([]point, 0)
+	for i := l.A; !i.Equal(l.B); i = i.Add(l.V) {
+		result = append(result, i)
+	}
+	result = append(result, l.B)
+	return result
+}
+
+type line struct {
+	A, B point
+	D    direction
+	V    point
+}
+
+func parsePoint(input string) point {
+	p := point{}
+	parts := strings.Split(input, ",")
+	p.I, _ = strconv.Atoi(parts[0])
+	p.J, _ = strconv.Atoi(parts[1])
+	return p
+}
+
+func parseLine(input string) line {
+	parts := strings.Fields(input)
+	l := line{
+		A: parsePoint(parts[0]),
+		B: parsePoint(parts[2]),
+		D: Other,
+	}
+	if l.A.I == l.B.I {
+		l.D = EastWest
+	}
+	if l.A.J == l.B.J {
+		l.D = NorthSouth
+	}
+	l.V = l.B.Sub(l.A).Norm()
+	return l
+}
+
 func part1(input []string) int {
-	return 0
+	hits := make(map[point]int)
+	overlaps := make(map[point]int)
+	for _, v := range input {
+		if v == "" {
+			continue
+		}
+		l := parseLine(v)
+		if l.D == Other {
+			continue
+		}
+		for _, p := range l.Trace() {
+			hits[p]++
+			if hits[p] > 1 {
+				overlaps[p]++
+			}
+		}
+	}
+	return len(overlaps)
 }
 
 func part2(input []string) int {
-	return 0
+	hits := make(map[point]int)
+	overlaps := make(map[point]int)
+	for _, v := range input {
+		if v == "" {
+			continue
+		}
+		l := parseLine(v)
+		for _, p := range l.Trace() {
+			hits[p]++
+			if hits[p] > 1 {
+				overlaps[p]++
+			}
+		}
+	}
+	return len(overlaps)
 }
--- cmd/day5/main_test.go
+++ cmd/day5/main_test.go
@@ -6,12 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []string{
+	"0,9 -> 5,9",
+	"8,0 -> 0,8",
+	"9,4 -> 3,4",
+	"2,2 -> 2,1",
+	"7,0 -> 7,4",
+	"6,4 -> 2,0",
+	"0,9 -> 2,9",
+	"3,4 -> 1,4",
+	"0,0 -> 8,8",
+	"5,5 -> 8,2",
+	"",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 5, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, 12, part2(input))
 }
