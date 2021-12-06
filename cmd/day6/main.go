361954056b7189ce186183b1820fe65e0c0c13b3
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-06 00:35:27 -0800

    day 6

--- cmd/day6/main.go
+++ cmd/day6/main.go
@@ -2,24 +2,67 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	raw, err := lib.GetInput("inputs/day6.txt")
 	if err != nil {
 		panic(err)
+	}
+	parts := strings.Split(raw[0], ",")
+	input := make([]int, len(parts))
+	for i, part := range parts {
+		input[i], _ = strconv.Atoi(part)
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
-func part1(input []string) int {
-	return 0
+func part1(fishes []int) int {
+	for day := 0; day < 80; day++ {
+		newFish := 0
+		for i, fish := range fishes {
+			if fish == 0 {
+				fishes[i] = 6
+				newFish++
+				continue
+			}
+			fishes[i]--
+		}
+		for i := 0; i < newFish; i++ {
+			fishes = append(fishes, 8)
+		}
+	}
+	return len(fishes)
 }
 
-func part2(input []string) int {
-	return 0
+func part2(fishes []int) int64 {
+	return simulate(fishes, 256)
+}
+
+func simulate(fishes []int, days int) int64 {
+	hist := make([]int, 9)
+
+	for _, fish := range fishes {
+		hist[fish]++
+	}
+
+	for day := 0; day < days; day++ {
+		zero := hist[0]
+		hist = append(hist[1:], zero)
+		hist[6] += zero
+		fmt.Println(day, hist)
+	}
+
+	sum := int64(0)
+	for _, count := range hist {
+		sum += int64(count)
+	}
+
+	return sum
 }
--- cmd/day6/main_test.go
+++ cmd/day6/main_test.go
@@ -6,12 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []int{3, 4, 3, 1, 2}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 5934, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, int64(5934), simulate(input, 80))
+	assert.Equal(t, int64(26984457539), part2(input))
 }
