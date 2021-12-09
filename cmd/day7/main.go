93963a792ed2deccd004425ed47691024b433e42
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2021-12-09 00:22:23 -0800

    day 7 completed

--- cmd/day7/main.go
+++ cmd/day7/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day7.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,10 +20,75 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
+func toIntSlice(input []string) []int {
+	result := make([]int, len(input))
+	for idx, val := range input {
+		result[idx], _ = strconv.Atoi(val)
+	}
+	return result
+}
+
+func abs(o int) int {
+	if o >= 0 {
+		return o
+	}
+	return -1 * o
+}
+
 func part1(input []string) int {
-	return 0
+	positions := toIntSlice(strings.Split(input[0], ","))
+
+	values := make(map[int]bool)
+
+	minFuelUsed := 0
+	for _, position := range positions {
+		values[position] = true
+		minFuelUsed += position
+	}
+
+	for value := range values {
+		totalFuel := 0
+		for _, position := range positions {
+			totalFuel += abs(position - value)
+		}
+		if totalFuel < minFuelUsed {
+			minFuelUsed = totalFuel
+		}
+	}
+
+	return minFuelUsed
+
 }
 
 func part2(input []string) int {
-	return 0
+	positions := toIntSlice(strings.Split(input[0], ","))
+
+	values := make(map[int]bool)
+
+	sort.IntSlice(positions).Sort()
+
+	maxDiff := positions[len(positions)-1] - positions[0]
+	lookupTable := make([]int, maxDiff+1)
+
+	for i := 1; i < len(lookupTable); i++ {
+		lookupTable[i] = i + lookupTable[i-1]
+	}
+
+	minFuelUsed := 1<<(bits.UintSize-1) - 1
+	for _, position := range positions {
+		values[position] = true
+	}
+
+	for value := 0; value <= positions[len(positions)-1]; value++ {
+		totalFuel := 0
+		for _, position := range positions {
+			dist := abs(position - value)
+			totalFuel += lookupTable[dist]
+		}
+		if totalFuel < minFuelUsed {
+			minFuelUsed = totalFuel
+		}
+	}
+
+	return minFuelUsed
 }
--- cmd/day7/main_test.go
+++ cmd/day7/main_test.go
@@ -6,12 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []string{
+	"16,1,2,0,4,2,7,1,2,14",
+	"",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 37, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, 168, part2(input))
 }
