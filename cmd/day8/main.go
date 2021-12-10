1b5e34325b76697ad33edfa011e7f68d20cb603c
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-10 00:19:15 -0800

    day 8 complete

--- cmd/day8/main.go
+++ cmd/day8/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day8.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,10 +18,135 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
+type noteEntry struct {
+	signalPatterns []signalReading
+	displays       []signalReading
+}
+
+type signalReading struct {
+	flags    uint
+	segments int
+}
+
+func encodeSignal(input string) signalReading {
+	segments := len(input)
+	val := uint(0)
+	for _, c := range input {
+		offset := c - 'a'
+		val = val | 1<<offset
+	}
+	return signalReading{
+		flags:    val,
+		segments: segments,
+	}
+}
+
+func (n noteEntry) descramble() int {
+	legend := make(map[uint]int, 10)
+	numbers := make([]uint, 10)
+
+	// first pass: lock in the unique segments
+	for _, signal := range n.signalPatterns {
+		switch signal.segments {
+		case 2:
+			legend[signal.flags] = 1
+			numbers[1] = signal.flags
+		case 3:
+			legend[signal.flags] = 7
+			numbers[7] = signal.flags
+		case 4:
+			legend[signal.flags] = 4
+			numbers[4] = signal.flags
+		case 7:
+			legend[signal.flags] = 8
+			numbers[8] = signal.flags
+		}
+	}
+
+	// second pass: use bitmasking to deduce the rest
+	// "mystery" bitvector given as bv
+	// 5-segment signals:
+	// 3 -> bv & bitvector[1] == bitvector[1]
+	// 2 -> bv &^ bitvector[4] has 3 bits set
+	// 5 -> not 3 or 2
+
+	// 6-segment signals:
+	// 9 -> bv & bitvector[4] == bitvector[4]
+	// 0 -> bv & bitvector[1] == bitvector[1]
+	// 6 -> not 9 or 0
+	for _, signal := range n.signalPatterns {
+		if _, ok := legend[signal.flags]; ok {
+			continue
+		}
+		switch signal.segments {
+		case 5:
+			if signal.flags&numbers[1] == numbers[1] {
+				legend[signal.flags] = 3
+			} else if masked := signal.flags &^ numbers[4]; bits.OnesCount(masked) == 3 {
+				legend[signal.flags] = 2
+			} else {
+				legend[signal.flags] = 5
+			}
+		case 6:
+			if signal.flags&numbers[4] == numbers[4] {
+				legend[signal.flags] = 9
+			} else if signal.flags&numbers[1] == numbers[1] {
+				legend[signal.flags] = 0
+			} else {
+				legend[signal.flags] = 6
+			}
+		}
+	}
+
+	return legend[n.displays[0].flags]*1000 +
+		legend[n.displays[1].flags]*100 +
+		legend[n.displays[2].flags]*10 +
+		legend[n.displays[3].flags]
+}
+
+func parseLine(input string) noteEntry {
+	parts := strings.Split(input, " | ")
+
+	n := noteEntry{
+		signalPatterns: make([]signalReading, 10),
+		displays:       make([]signalReading, 4),
+	}
+
+	for i, pattern := range strings.Fields(parts[0]) {
+		n.signalPatterns[i] = encodeSignal(pattern)
+	}
+
+	for i, display := range strings.Fields(parts[1]) {
+		n.displays[i] = encodeSignal(display)
+	}
+
+	return n
+}
+
 func part1(input []string) int {
-	return 0
+	acc := 0
+	for _, s := range input {
+		if s == "" {
+			continue
+		}
+		n := parseLine(s)
+		for _, display := range n.displays {
+			if (display.segments >= 2 && display.segments <= 4) || display.segments == 7 {
+				acc++
+			}
+		}
+	}
+	return acc
 }
 
 func part2(input []string) int {
-	return 0
+	acc := 0
+	for _, s := range input {
+		if s == "" {
+			continue
+		}
+		n := parseLine(s)
+		acc += n.descramble()
+	}
+	return acc
 }
--- cmd/day8/main_test.go
+++ cmd/day8/main_test.go
@@ -6,12 +6,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []string{
+	"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+	"edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+	"fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+	"fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+	"aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+	"fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+	"dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+	"bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+	"egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+	"gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+	"",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 26, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, 61229, part2(input))
+}
+
+func TestEncodeSignal(t *testing.T) {
+	type testCase struct {
+		input  string
+		result uint
+	}
+
+	testCases := []testCase{
+		{"be", uint(18)},
+		{"cfbegad", uint(127)},
+		{"cbdgef", uint(126)},
+		{"fgaecd", uint(125)},
+		{"cgeb", uint(86)},
+		{"fdcge", uint(124)},
+	}
+
+	for i, tc := range testCases {
+		assert.Equal(t, tc.result, encodeSignal(tc.input).flags, "test case %d failed: %s", i, tc.input)
+	}
 }
