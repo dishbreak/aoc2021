4b170174e242cadd2aab5bb1ea8239221c8f769b
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-10 01:33:49 -0800

    finished day 9

--- cmd/day9/main.go
+++ cmd/day9/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"sort"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day9.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,10 +18,92 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
+func toSpace(input []string) map[image.Point]int {
+	space := make(map[image.Point]int, len(input)*len(input[0]))
+	for x, s := range input {
+		for y, c := range s {
+			space[image.Point{x, y}] = int(c - '0')
+		}
+	}
+	return space
+}
+
+var neighbors []image.Point = []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+func isLocalMin(space map[image.Point]int, p image.Point) bool {
+	v, ok := space[p]
+	if !ok {
+		return false
+	}
+
+	for _, n := range neighbors {
+		val, ok := space[p.Add(n)]
+		if !ok {
+			continue
+		}
+		if val <= v { // tricksy...
+			return false
+		}
+	}
+	return true
+}
+
 func part1(input []string) int {
-	return 0
+	space := toSpace(input[:len(input)-1])
+
+	acc := 0
+	for p, v := range space {
+		if isLocalMin(space, p) {
+			acc += (v + 1)
+		}
+	}
+	return acc
 }
 
 func part2(input []string) int {
-	return 0
+	space := toSpace(input[:len(input)-1])
+
+	lowPoints := make([]image.Point, 0)
+
+	for p := range space {
+		if isLocalMin(space, p) {
+			lowPoints = append(lowPoints, p)
+		}
+	}
+
+	basins := make([]int, len(lowPoints))
+
+	for i, lp := range lowPoints {
+		acc := 0
+		seen := make(map[image.Point]bool, len(space))
+		seen[lp] = true
+		q := make([]image.Point, 1)
+		q[0] = lp
+
+		for len(q) > 0 {
+			p := q[0]
+			q = q[1:]
+
+			val, ok := space[p]
+			if !ok || val == 9 {
+				continue
+			}
+
+			acc++
+			for _, n := range neighbors {
+				next := p.Add(n)
+				if _, ok := seen[next]; ok {
+					continue
+				}
+				seen[next] = true
+				q = append(q, p.Add(n))
+			}
+		}
+		basins[i] = acc
+	}
+	sort.Slice(basins, func(i, j int) bool {
+		return basins[i] > basins[j]
+	})
+
+	return basins[0] * basins[1] * basins[2]
 }
--- cmd/day9/main_test.go
+++ cmd/day9/main_test.go
@@ -6,12 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []string{
+	"2199943210",
+	"3987894921",
+	"9856789892",
+	"8767896789",
+	"9899965678",
+	"",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 15, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, 1134, part2(input))
 }
