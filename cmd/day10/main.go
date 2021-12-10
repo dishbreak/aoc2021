95b6f5658776ef3399f77fd14d07e34da36f930f
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-10 01:53:38 -0800

    day 10 part 1

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day10.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,8 +16,44 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
-func part1(input []string) int {
+var scores = map[rune]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+func evaluateLine(line string) int {
+	s := make([]rune, 0)
+	for _, c := range line {
+		switch c {
+		case '(':
+			s = append(s, ')')
+		case '[':
+			s = append(s, ']')
+		case '{':
+			s = append(s, '}')
+		case '<':
+			s = append(s, '>')
+		default:
+			if s[len(s)-1] != c {
+				return scores[c]
+			}
+			s = s[:len(s)-1]
+		}
+	}
 	return 0
+}
+
+func part1(input []string) int {
+	acc := 0
+	for _, line := range input {
+		if line == "" {
+			continue
+		}
+		acc += evaluateLine(line)
+	}
+	return acc
 }
 
 func part2(input []string) int {
