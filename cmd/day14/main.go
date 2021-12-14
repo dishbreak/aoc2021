c9ffc47b0d9464485838e0f6cecad177a4cfa90e
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-14 00:49:51 -0800

    finished day 14

--- cmd/day14/main.go
+++ cmd/day14/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInputAsSections("inputs/day14.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,10 +18,102 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
-func part1(input []string) int {
-	return 0
+type polymerChain struct {
+	pairs map[string]int
+	chars map[byte]int
 }
 
-func part2(input []string) int {
-	return 0
+type insertionRule struct {
+	insertChar   byte
+	matchingPair string
+	newPairs     []string
+}
+
+func parsePolymerChain(input string) polymerChain {
+	p := polymerChain{
+		pairs: make(map[string]int),
+		chars: make(map[byte]int),
+	}
+
+	for i := 0; i < len(input)-1; i++ {
+		p.pairs[input[i:i+2]]++
+		p.chars[input[i]]++
+	}
+	p.chars[input[len(input)-1]]++
+
+	return p
+}
+
+func parseInsertionRule(input string) insertionRule {
+	parts := strings.Fields(input)
+
+	r := insertionRule{
+		insertChar:   parts[2][0],
+		matchingPair: parts[0],
+	}
+
+	first, second := r.matchingPair[0], r.matchingPair[1]
+	r.newPairs = []string{
+		string([]byte{first, r.insertChar}),
+		string([]byte{r.insertChar, second}),
+	}
+
+	return r
+}
+
+func simulate(input [][]string, steps int) int {
+	ruleSet := make(map[string]insertionRule, len(input[1]))
+	p := parsePolymerChain(input[0][0])
+
+	for _, line := range input[1] {
+		r := parseInsertionRule(line)
+		ruleSet[r.matchingPair] = r
+	}
+
+	for i := 0; i < steps; i++ {
+		q := make(map[string]insertionRule)
+
+		for pair := range p.pairs {
+			if rule, ok := ruleSet[pair]; !ok {
+				continue
+			} else {
+				q[pair] = rule
+			}
+		}
+
+		nextChain := make(map[string]int)
+
+		for pair, val := range p.pairs {
+			if _, ok := q[pair]; ok {
+				continue
+			}
+			nextChain[pair] = val
+		}
+
+		for pair, rule := range q {
+			val := p.pairs[pair]
+
+			p.chars[rule.insertChar] += val
+			nextChain[rule.newPairs[0]] += val
+			nextChain[rule.newPairs[1]] += val
+		}
+		p.pairs = nextChain
+	}
+
+	counts := make([]int, 0)
+
+	for _, val := range p.chars {
+		counts = append(counts, val)
+	}
+
+	sort.IntSlice(counts).Sort()
+	return counts[len(counts)-1] - counts[0]
+}
+
+func part1(input [][]string) int {
+	return (simulate(input, 10))
+}
+
+func part2(input [][]string) int {
+	return simulate(input, 40)
 }
--- cmd/day14/main_test.go
+++ cmd/day14/main_test.go
@@ -6,12 +6,45 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = [][]string{
+	{
+		"NNCB",
+	},
+	{
+		"CH -> B",
+		"HH -> N",
+		"CB -> H",
+		"NH -> C",
+		"HB -> C",
+		"HC -> B",
+		"HN -> C",
+		"NN -> C",
+		"BH -> H",
+		"NC -> B",
+		"NB -> B",
+		"BN -> B",
+		"BB -> N",
+		"BC -> B",
+		"CC -> N",
+		"CN -> C",
+	},
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 1588, part1(input))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, part2(input))
+	assert.Equal(t, 2188189693529, part2(input))
+}
+
+func TestInsertionRule(t *testing.T) {
+	ruleString := "FG -> H"
+	expected := insertionRule{
+		insertChar:   'H',
+		matchingPair: "FG",
+		newPairs:     []string{"FH", "HG"},
+	}
+
+	assert.Equal(t, expected, parseInsertionRule(ruleString))
 }
