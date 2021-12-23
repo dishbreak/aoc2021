3c04705237c8f432451b24c365a46462db7f57e2
Vishal Kotcherlakota <dishbreak@gmail.com>
2021-12-23 14:48:02 -0800

    day 18: finished tree parse and stringify

--- cmd/day18/main.go
+++ cmd/day18/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"strconv"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
@@ -18,22 +21,91 @@ func main() {
 }
 
 type node struct {
-	value       int
-	left, right *node
+	value             int
+	left, right, root *node
+}
+
+func NodeFromString(input string) *node {
+	buf := strings.NewReader(input)
+	n := nodeFromReader(buf)
+	return n
+}
+
+func nodeFromReader(buf io.ByteReader) *node {
+	result := &node{
+		value: -1,
+	}
+
+	if c, _ := buf.ReadByte(); c == '[' {
+		result.left = nodeFromReader(buf)
+	} else if v := c - '0'; v <= 9 {
+		result.value = int(v)
+		return result
+	}
+
+	if c, _ := buf.ReadByte(); c != ',' {
+		panic(errors.New("expected comma"))
+	}
+
+	result.right = nodeFromReader(buf)
+	if c, _ := buf.ReadByte(); c != ']' {
+		panic(errors.New("expected closing brace"))
+	}
+	return result
+}
+
+func (n *node) IsLeaf() bool {
+	return n.left == nil && n.right == nil
+}
+
+func (n *node) Explode() bool {
+
+	type frame struct {
+		n     *node
+		level int
+	}
+	s := make([]frame, 0)
+
+	s = append(s, frame{n.right, 1})
+	s = append(s, frame{n.left, 1})
+
+	for len(s) > 0 {
+		p := s[len(s)-1]
+		s = s[1:]
+
+		if p.level > 4 && p.n.IsLeaf() {
+			if t := p.n.root.root.left; t != nil && t.IsLeaf() {
+				t.value += p.n.left.value
+			}
+			if t := p.n.root.root.right; t != nil && t.IsLeaf() {
+				t.value += p.n.right.value
+			}
+			return true
+		}
+
+		if p.n.IsLeaf() {
+			continue
+		}
+
+		s = append(s, frame{p.n.right, p.level + 1})
+		s = append(s, frame{p.n.left, p.level + 1})
+	}
+
+	return false
 }
 
 func add(l, r *node) *node {
 	result := &node{
 		value: -1,
 	}
-	result.left = l
-	result.right = r
+	l.root, r.root = result, result
+	result.left, result.right = l, r
 
 	return result
 }
 
 func (n *node) String() string {
-	if n.value != -1 {
+	if n.IsLeaf() {
 		return strconv.Itoa(n.value)
 	}
 	return fmt.Sprintf("[%s,%s]", n.left.String(), n.right.String())
--- cmd/day18/main_test.go
+++ cmd/day18/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,59 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	assert.Equal(t, 0, part2(input))
+}
+
+func newNode(l, r int) *node {
+	return add(
+		&node{value: l},
+		&node{value: r},
+	)
+}
+
+func TestAdd(t *testing.T) {
+	type testCase struct {
+		operands []*node
+		expected string
+	}
+
+	testCases := []testCase{
+		{
+			operands: []*node{
+				newNode(1, 1),
+				newNode(2, 2),
+				newNode(3, 3),
+				newNode(4, 4),
+			},
+			expected: "[[[[1,1],[2,2]],[3,3]],[4,4]]",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			acc := tc.operands[0]
+			for _, other := range tc.operands[1:] {
+				acc = add(acc, other)
+			}
+			assert.Equal(t, tc.expected, acc.String())
+		})
+	}
+}
+
+func TestNodeFromString(t *testing.T) {
+	tests := []string{
+		"[1,2]",
+		"[[1,2],3]",
+		"[9,[8,7]]",
+		"[[1,9],[8,5]]",
+		"[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
+		"[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
+		"[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			n := NodeFromString(test)
+			assert.Equal(t, test, n.String())
+		})
+	}
 }
